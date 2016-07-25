9146b403fe7ef815871bc620fa85167cdd830183
Erica Winberry <elaewin@gmail.com>
2016-07-25 14:19:03 -0700

    complete demo from lecture

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,10 +1,49 @@
+'use strict';
+
 (function(module) {
   var zip = {};
-  
-  getData = function() {
+  var neighborhoodTotals = {};
+  zip.topFive = [];
+
+  var getData = function() {
+    function justTheGoodProps(obj) {
+      return {
+        zipCode: obj.properties.zip,
+        neighborhood: !!obj.properties.neighborhood ? obj.properties.neighborhood.split(',')[0] : null,
+        address: !!obj.properties.address ? obj.properties.address : null,
+        coordinates: {
+          lat: obj.geometry.coordinates[1],
+          lng: obj.geometry.coordinates[0]
+        }
+      };
+    }
+
     $.getJSON('/data/manhattan.json', function(data) {
-      // TODO: start here!
+      data.features.map(justTheGoodProps)
+      .forEach(buildZipsDictionary);
+      generateTopFive(neighborhoodTotals);
     });
+  };
+
+  function buildZipsDictionary(zipObj) {
+    var curNeighb = zipObj.neighborhood;
+    neighborhoodTotals[curNeighb] ?
+      neighborhoodTotals[curNeighb] ++ :
+      neighborhoodTotals[curNeighb] = 1;
+  }
+
+  function generateTopFive(obj) {
+    for(var neighborhood in obj) {
+      zip.topFive.push({
+        neighborhood: neighborhood,
+        totalZips: obj[neighborhood]
+      });
+    }
+    zip.topFive.sort(descending).splice(5);
+  }
+
+  var descending = function(curNeighb, nextNeighb) {
+    return nextNeighb.totalZips - curNeighb.totalZips;
   };
 
   getData();
