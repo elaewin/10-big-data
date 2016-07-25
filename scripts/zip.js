ac0fb089bb863817d06447766caadaf2e4c27f0a
Erica Winberry <elaewin@gmail.com>
2016-07-24 17:59:55 -0700

    getting all of the data, now working on getting the top 5 list.

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,12 +1,42 @@
 (function(module) {
   var zip = {};
-  
-  getData = function() {
+
+  zip.locations = [];
+
+  zip.getData = function() {
     $.getJSON('/data/manhattan.json', function(data) {
-      // TODO: start here!
+      zip.locations = data.features.map(function(allData) {
+        var newObj = {
+          zipcode: allData.properties.zip,
+          neighborhood: allData.properties.neighborhood.split(',')[0],
+          address: allData.properties.address,
+          coordinates: {
+            lat: allData.geometry.coordinates[1],
+            lng: allData.geometry.coordinates[0]
+          }
+        };
+        console.log(newObj);
+        return newObj;
+      }).reduce(function(uniqueNeighborhoods, currentZip, index, array) {
+        if(uniqueNeighborhoods.indexOf(currentZip.zipcode) === -1) {
+          uniqueNeighborhoods.push(currentZip);
+        }
+        return uniqueNeighborhoods;
+      }, []);
+      console.log(zip.locations.length);
+      return zip.locations;
     });
   };
 
-  getData();
+  // zip.getTopFive = function(datasetEntered) {
+  //   var results = getData().map(function(neighborhood) {
+  //     return {
+  //       place: neighborhood,
+  //       total:
+  //     }
+  //   })
+  // }
+
+  zip.getData();
   module.zip = zip;
 })(window);
